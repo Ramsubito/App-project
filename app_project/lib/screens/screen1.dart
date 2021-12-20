b7b8eafa2b93318df0afec934548d6ae44298a76
DESKTOP-C8H0847\Ramsubo <marctripleh@gmail.com>
2021-12-20 18:10:29 +0100

    Buttons and search bar second screen

--- app_project/lib/screens/screen1.dart
+++ app_project/lib/screens/screen1.dart
@@ -17,46 +17,207 @@ class _Screen1State extends State<Screen1> {
       appBar: AppBar(
         title: const Text('Prices Sceen'),
       ),
-      backgroundColor: Colors.transparent,
+      backgroundColor: Colors.white,
       body: Center(
           child: Column(
         //mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              height: 225,
-              width: 400,
-              decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 234, 61, 78),
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(30),
-                      bottomRight: Radius.circular(30)),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey,
-                        spreadRadius: 1,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3)),
-                  ]),
-              child: Column(
-                children: const <Widget>[
-                  Padding(padding: EdgeInsets.only(top: 40)),
-                  Icon(
-                    Icons.settings,
-                    color: Colors.black,
-                    size: 50,
+        children: <Widget>[
+          Container(
+            height: 175,
+            width: 400,
+            decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 234, 61, 78),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30)),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey,
+                      spreadRadius: 1,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3)),
+                ]),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 300,
+                  height: 50,
+                  margin: const EdgeInsets.only(left: 35, top: 65),
+                  child: Row(
+                    children: const [
+                      Text(
+                        'Search...',
+                        style: TextStyle(fontSize: 24, color: Colors.grey),
+                      ),
+                      Padding(padding: EdgeInsets.only(left: 150)),
+                      Icon(
+                        Icons.search,
+                        size: 42,
+                      )
+                    ],
                   ),
-                  Padding(padding: EdgeInsets.only(top: 20)),
-                  Text(
-                    'New Screen',
-                    style: TextStyle(fontSize: 60, fontWeight: FontWeight.w400),
+                  padding: const EdgeInsets.only(top: 3, left: 6),
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.circular(20), //Search your trip add icon
+                    color: Colors.grey.shade200,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
+          Container(
+            height: 80,
+            width: 320,
+            margin: const EdgeInsets.only(top: 25),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.grey,
+                      spreadRadius: 2,
+                      blurRadius: 7,
+                      offset: Offset(0, 6)),
+                ]),
+            child: ElevatedButton(
+                onPressed: OnPressedButon2,
+                child: const Text(
+                  'Game1',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 20,
+                    fontFamily: 'Swiss',
+                  ),
+                ),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 245, 245, 245)),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 245, 245, 245)),
+                  fixedSize:
+                      MaterialStateProperty.all<Size>(const Size(250, 50)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                )),
+          ),
+          Container(
+            height: 80,
+            width: 320,
+            margin: const EdgeInsets.only(top: 25),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.grey,
+                      spreadRadius: 2,
+                      blurRadius: 7,
+                      offset: Offset(0, 6)),
+                ]),
+            child: ElevatedButton(
+                onPressed: OnPressedButon2,
+                child: const Text(
+                  'Game2',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 20,
+                    fontFamily: 'Swiss',
+                  ),
+                ),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 245, 245, 245)),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 245, 245, 245)),
+                  fixedSize:
+                      MaterialStateProperty.all<Size>(const Size(250, 50)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                )),
+          ),
+          Container(
+            height: 80,
+            width: 320,
+            margin: const EdgeInsets.only(top: 25),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.grey,
+                      spreadRadius: 2,
+                      blurRadius: 7,
+                      offset: Offset(0, 6)),
+                ]),
+            child: ElevatedButton(
+                onPressed: OnPressedButon2,
+                child: const Text(
+                  'Game3',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 20,
+                    fontFamily: 'Swiss',
+                  ),
+                ),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 245, 245, 245)),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 245, 245, 245)),
+                  fixedSize:
+                      MaterialStateProperty.all<Size>(const Size(250, 50)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                )),
+          ),
+          Container(
+            height: 80,
+            width: 320,
+            margin: const EdgeInsets.only(top: 25),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.grey,
+                      spreadRadius: 2,
+                      blurRadius: 7,
+                      offset: Offset(0, 6)),
+                ]),
+            child: ElevatedButton(
+                onPressed: OnPressedButon2,
+                child: const Text(
+                  'Game4',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 20,
+                    fontFamily: 'Swiss',
+                  ),
+                ),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 245, 245, 245)),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 245, 245, 245)),
+                  fixedSize:
+                      MaterialStateProperty.all<Size>(const Size(250, 50)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                )),
+          )
         ],
       )),
     );
   }
 }
+
+void OnPressedButon2() {}

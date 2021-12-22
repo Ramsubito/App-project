1b31a774ffdacefc54e869bcabecd4e69fec4280
DESKTOP-C8H0847\Ramsubo <marctripleh@gmail.com>
2021-12-22 20:11:22 +0100

    Revert "ESO"
    This reverts commit 7841e67262d879249c388e68d1f4c0bd9a2cb3c4.

--- app_project/lib/screens/screen3.dart
+++ app_project/lib/screens/screen3.dart
@@ -1,11 +1,8 @@
-// ignore: import_of_legacy_library_into_null_safe
-import 'dart:html';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:app_project/Info/mockedoffers.dart';
 import 'package:app_project/models/offers.dart';
-import 'package:app_project/main.dart';
+//import 'package:app_project/main.dart';
 
 class Screen3 extends StatefulWidget {
   const Screen3({Key? key, required this.title}) : super(key: key);
@@ -17,30 +14,28 @@ class Screen3 extends StatefulWidget {
 }
 
 class _Screen3State extends State<Screen3> {
-  //List<Offers> offers = Info.getMockedOffers().cast<Offers>();
+  List<Offers> offers = Info.getMockedOffers().cast<Offers>();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: StreamBuilder(
-            stream: Firestore.instance.collection('Offers').snapshots(),
-            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              List<DocumentSnapshot> doc = snapshot.data!.documents;
-              return ListView.builder(
-                  itemCount: doc.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    Map<String, dynamic> data = doc[index].data;
-                    return ListTile(
-                      title: Text(data['Game']),
-                    );
-                  });
-            }),
-      ),
+    return Scaffold(
+      body: StreamBuilder(
+          stream: Firestore.instance.collection('Offers').snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            List<DocumentSnapshot> docs = snapshot.data.documents;
+            return ListView.builder(
+                itemCount: docs.length,
+                itemBuilder: (context, index) {
+                  Map<String, dynamic> data = docs[index].data;
+                  return ListTile(
+                    title: Text(data['Game']),
+                  );
+                });
+          }),
     );
   }
 }

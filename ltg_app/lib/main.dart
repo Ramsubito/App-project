9d4e9ed536299d7d62d7707a86a39e46e162373a
DESKTOP-C8H0847\Ramsubo <marctripleh@gmail.com>
2022-01-22 20:35:25 +0100

    Added logic Firebase test

--- ltg_app/lib/main.dart
+++ ltg_app/lib/main.dart
@@ -5,8 +5,11 @@ import 'package:ltg_app/screens/screen2.dart';
 import 'package:ltg_app/screens/screen3.dart';
 import 'package:flutter/material.dart';
 import 'package:ltg_app/screens/screen1.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const LTG_App());
 }
 
--- ltg_app/lib/screens/screen3.dart
+++ ltg_app/lib/screens/screen3.dart
@@ -1,4 +1,6 @@
 //import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ltg_app/Info/mockedoffers.dart';
 import 'package:ltg_app/models/offers.dart';
@@ -17,11 +19,36 @@ class _Screen3State extends State<Screen3> {
   List<Offers> offers = Info.getMockedOffers().cast<Offers>();
   @override
   Widget build(BuildContext context) {
+    // ignore: dead_code
     return Scaffold(
-        body: Center(
-      child: Text(
-          'Hem tingut problemes amb el Firestore i finalment l hem eliminat del projecte, per la seguent entrega estará implementat'),
-    ));
+      appBar: AppBar(
+        title: const Text("Firebase"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => FirebaseFirestore.instance
+            .collection('testing')
+            .add({'timestamp': Timestamp.fromDate(DateTime.now())}),
+        child: Icon(Icons.add),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('testing').snapshots(),
+          builder: (
+            BuildContext context,
+            AsyncSnapshot<QuerySnapshot> snapshot,
+          ) {
+            if (!snapshot.hasData) return const SizedBox.shrink();
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                final docData = snapshot.data!.docs[index];
+                final dateTime = (docData["timestamp"] as Timestamp).toDate();
+                return ListTile(
+                  title: Text(dateTime.toString()),
+                );
+              },
+            );
+          }),
+    );
   }
 }
 

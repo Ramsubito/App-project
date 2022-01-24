ae4190dcc2b55d4cc9ae962a4f979b58d2ca8174
DESKTOP-C8H0847\Ramsubo <marctripleh@gmail.com>
2022-01-24 20:18:21 +0100

    Registration/Login Implemented

--- ltg_app/lib/main.dart
+++ ltg_app/lib/main.dart
@@ -6,11 +6,12 @@ import 'package:ltg_app/screens/screen3.dart';
 import 'package:flutter/material.dart';
 import 'package:ltg_app/screens/screen1.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:ltg_app/widgets/auth_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(LTG_App());
+  runApp(AuthGate(app: LTG_App()));
 }
 
 // ignore: camel_case_types
@@ -256,7 +257,7 @@ class LTG_App extends StatelessWidget {
                                     ),
                                   )),
                             ),
-                          )
+                          ),
                         ],
                       )),
                     ));
